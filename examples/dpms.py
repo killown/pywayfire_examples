abafe776f3b50be3643e0aa46953daa183bf010f
killown <systemofdown@gmail.com>
2026-01-27 13:17:03 -0300

    Update dpms.py

--- examples/dpms.py
+++ examples/dpms.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2025 Thiago <systemofdown@gmail.com> (killown)
+# Copyright (c) 2025 Thiago <killown.matrix@gmail.com> (killown)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
